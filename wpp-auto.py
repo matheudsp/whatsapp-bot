0c6a8480d6a6491d5240b6644367e36fd17deedf
matheudsp <mdsp.personal@gmail.com>
2021-11-02 22:17:41 -0300

    build v1.1

--- wpp-auto.py
+++ wpp-auto.py
@@ -7,12 +7,14 @@ from selenium.webdriver.common.keys import Keys
 
 #Whatsapp Login ----------------------------------
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://web.whatsapp.com/')
-time.sleep(30)
+driver.get('https://web.whatsapp.com/') #abrir o site   
+time.sleep(25) #espera pra escanear o qr-code
 
 #Definir ------------------------------------
-contatos = ['Quick Notes']
-mensagem = 'Esta mensagem foi enviada por um script feito em python.'
+contatos = ['Quick Notes'] 
+mensagem1 = 'Fala '
+mensagem2 = ' , segue minha playlist no spotify! https://open.spotify.com/playlist/1K04fOBfMaGxt47Xp6KvEB'
+midia = "C:/workspace/wpp-auto/images/rock-spotify.jpeg"
 
 #WppWeb> Buscar ctt/grupo > Enviar ----------------------------------
 def buscar_contato(contato):
@@ -22,14 +24,25 @@ def buscar_contato(contato):
     campo_pesquisa.send_keys(contato)
     campo_pesquisa.send_keys(Keys.ENTER)
 
-def enviar_mensagem(mensagem):
+def enviar_mensagem(mensagem1,mensagem2):
     campo_mensagem = driver.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
     campo_mensagem[1].click()
     time.sleep(3)
-    campo_mensagem[1].send_keys(mensagem)
+    campo_mensagem[1].send_keys(str(mensagem1) + str(contato) + str(mensagem2))
     campo_mensagem[1].send_keys(Keys.ENTER)
 
+def enviar_midia(midia):
+    driver.find_element_by_css_selector("span[data-icon='clip']").click()
+    attach = driver.find_element_by_css_selector("input[type='file']")
+    attach.send_keys(midia)
+    time.sleep(3)
+    send = driver.find_element_by_css_selector("span[data-icon='send']")
+    send.click()
+    time.sleep(5)    
+
 for contato in contatos:
+    
     buscar_contato(contato)
-    enviar_mensagem(mensagem)
+    enviar_midia(midia) 
+    enviar_mensagem(mensagem1,mensagem2)      
 
